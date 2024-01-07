baa178395d208aad1c096e8946625e0af26acd4c
Your Name <vishwaaps3@gmail.com>
2024-01-07 09:43:57 +0530

    Update paginationView.js

--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -1,21 +1,78 @@
 import View from "./View.js";
-import previewView from "./previewView.js";
 import icons from "url:../../img/icons.svg"; // Parcel 2
 
-class BookmarksView extends View {
-  _parentElement = document.querySelector(".bookmarks__list");
-  _errorMessage = "No bookmarks yet. Find a nice recipe and bookmark it ;)";
-  _message = "";
+class PaginationView extends View {
+  _parentElement = document.querySelector(".pagination");
 
-  addHandlerRender(handler) {
-    window.addEventListener("load", handler);
+  addHandlerClick(handler) {
+    this._parentElement.addEventListener("click", function (e) {
+      const btn = e.target.closest(".btn--inline");
+      if (!btn) return;
+
+      const goToPage = +btn.dataset.goto;
+      handler(goToPage);
+    });
   }
 
   _generateMarkup() {
-    return this._data
-      .map((bookmark) => previewView.render(bookmark, false))
-      .join("");
+    const curPage = this._data.page;
+    const numPages = Math.ceil(
+      this._data.results.length / this._data.resultsPerPage
+    );
+
+    // Page 1, and there are other pages
+    if (curPage === 1 && numPages > 1) {
+      return `
+        <button data-goto="${
+          curPage + 1
+        }" class="btn--inline pagination__btn--next">
+          <span>Page ${curPage + 1}</span>
+          <svg class="search__icon">
+            <use href="${icons}#icon-arrow-right"></use>
+          </svg>
+        </button>
+      `;
+    }
+
+    // Last page
+    if (curPage === numPages && numPages > 1) {
+      return `
+        <button data-goto="${
+          curPage - 1
+        }" class="btn--inline pagination__btn--prev">
+          <svg class="search__icon">
+            <use href="${icons}#icon-arrow-left"></use>
+          </svg>
+          <span>Page ${curPage - 1}</span>
+        </button>
+      `;
+    }
+
+    // Other page
+    if (curPage < numPages) {
+      return `
+        <button data-goto="${
+          curPage - 1
+        }" class="btn--inline pagination__btn--prev">
+          <svg class="search__icon">
+            <use href="${icons}#icon-arrow-left"></use>
+          </svg>
+          <span>Page ${curPage - 1}</span>
+        </button>
+        <button data-goto="${
+          curPage + 1
+        }" class="btn--inline pagination__btn--next">
+          <span>Page ${curPage + 1}</span>
+          <svg class="search__icon">
+            <use href="${icons}#icon-arrow-right"></use>
+          </svg>
+        </button>
+      `;
+    }
+
+    // Page 1, and there are NO other pages
+    return "";
   }
 }
 
-export default new BookmarksView();
+export default new PaginationView();
